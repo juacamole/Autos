446554c5ad0adf7832e6e37a66c557a350881ca2
LUEJ9 <juan.luethy@coop.ch>
2023-11-08 10:27:42 +0100

    added Cars Gemera and regera and starting + velocity gain mechanism

--- src/main/java/org/example/Autos.java
+++ src/main/java/org/example/Autos.java
@@ -1,12 +1,13 @@
 package org.example;
 
-
 public class Autos {
     public static void main(String[] args) {
-
-        Car myCar = new Car();
-
-
+        Car myCar = new Car ("koenigsegg", "regera", "grau", 2015, 400);
+        myCar.startup("koenigsegg", "regera");
+        Car fastCar = new Car ("koenigsegg", "gemera", "grün", 2023, 550);
+        myCar.beschleunigen("regera", 400);
+        fastCar.startup("koenigsegg", "gemera");
+        fastCar.beschleunigen("gemera", 550);
 
     }
 }
--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,9 +1,22 @@
 package org.example;
 
 public class Car {
+    String brand;
+    String model;
+    String color;
+    int year;
+    int velocity;
+    Car(String brand, String model, String color, int year, int velocity) {
 
-String marke;
-int baujahr;
-int preis;
-int ps;
-}
+    }
+
+    public void startup(String brand, String model) {
+        System.out.println("The " + brand + " " + model + " is starting.");
+    }
+    public void beschleunigen(String model, int velocity) {
+        int i = 0;
+        while(i<=velocity){
+            System.out.println("velocity of "+  model +" : " + i);
+            i += 10;
+        }
+    }}
